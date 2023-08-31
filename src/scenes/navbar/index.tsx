eb737c4977faf056ffdce55e7ef49bbc758f3d12
German Kosach <fuerstein@yandex.ru>
2023-08-31 18:34:02 +0300

    frontend finisinh

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
-import PixIcon from "@mui/icons-material/Pix"
+import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin"
 import { Box, Typography, useTheme } from "@mui/material"
 import FlexBetween from "@/components/FlexBetween"
 
@@ -13,9 +13,9 @@ const Navbar = (props: Props) => {
     <FlexBetween mb='0.25rem' p='0.5rem 0rem' color={palette.grey[300]}>
       {/* LEFT SIDE */}
       <FlexBetween gap='0.75rem'>
-        <PixIcon sx={{ fontSize: "28px" }} />
+        <CurrencyBitcoinIcon sx={{ fontSize: "28px" }} />
         <Typography variant='h4' fontSize='16px'>
-          Finanseer
+          PredictAI
         </Typography>
       </FlexBetween>
 
--- src/scenes/predictions/index.tsx
+++ src/scenes/predictions/index.tsx
@@ -1,53 +1,121 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import PixIcon from "@mui/icons-material/Pix"
-import { Box, Typography, useTheme } from "@mui/material"
+import DashboardBox from "@/components/DashboardBox"
 import FlexBetween from "@/components/FlexBetween"
+import { useGetKpisQuery } from "@/state/api"
+import { Box, Button, Typography, useTheme } from "@mui/material"
+import React, { useMemo, useState } from "react"
+import {
+  CartesianGrid,
+  Label,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts"
+import regression, { DataPoint } from "regression"
 
-type Props = {}
-
-const Navbar = (props: Props) => {
+const Predictions = () => {
   const { palette } = useTheme()
-  const [selected, setSelected] = useState("dashboard")
-  return (
-    <FlexBetween mb='0.25rem' p='0.5rem 0rem' color={palette.grey[300]}>
-      {/* LEFT SIDE */}
-      <FlexBetween gap='0.75rem'>
-        <PixIcon sx={{ fontSize: "28px" }} />
-        <Typography variant='h4' fontSize='16px'>
-          Finanseer
-        </Typography>
-      </FlexBetween>
+  const [isPredictions, setIsPredictions] = useState(false)
+  const { data: kpiData } = useGetKpisQuery()
 
-      {/* RIGHT SIDE */}
-      <FlexBetween gap='2rem'>
-        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
-          <Link
-            to='/'
-            onClick={() => setSelected("dashboard")}
-            style={{
-              color: selected === "dashboard" ? "inherit" : palette.grey[700],
-              textDecoration: "inherit",
-            }}
-          >
-            dashboard
-          </Link>
+  const formattedData = useMemo(() => {
+    if (!kpiData) return []
+    const monthData = kpiData[0].monthlyData
+
+    const formatted: Array<DataPoint> = monthData.map(
+      ({ revenue }, i: number) => {
+        return [i, revenue]
+      }
+    )
+    const regressionLine = regression.linear(formatted)
+
+    return monthData.map(({ month, revenue }, i: number) => {
+      return {
+        name: month,
+        "Actual Revenue": revenue,
+        "Regression Line": regressionLine.points[i][1],
+        "Predicted Revenue": regressionLine.predict(i + 12)[1],
+      }
+    })
+  }, [kpiData])
+
+  return (
+    <DashboardBox width='100%' height='100%' p='1rem' overflow='hidden'>
+      <FlexBetween m='1rem 2.5rem' gap='1rem'>
+        <Box>
+          <Typography variant='h3'>Revenue and Predictions</Typography>
+          <Typography variant='h6'>
+            charted revenue and predicted revenue based on a simple linear
+            regression model
+          </Typography>
         </Box>
-        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
-          <Link
-            to='/predictions'
-            onClick={() => setSelected("predictions")}
-            style={{
-              color: selected === "predictions" ? "inherit" : palette.grey[700],
-              textDecoration: "inherit",
-            }}
-          >
-            predictions
-          </Link>
-        </Box>
+        <Button
+          onClick={() => setIsPredictions(!isPredictions)}
+          sx={{
+            color: palette.grey[900],
+            backgroundColor: palette.grey[700],
+            boxShadow: "0.1rem 0.1rem 0.1rem 0.1rem rgba(0,0,0,.4)",
+          }}
+        >
+          Show Predicted Revenue for Next Year
+        </Button>
       </FlexBetween>
-    </FlexBetween>
+      <ResponsiveContainer width='100%' height='100%'>
+        <LineChart
+          data={formattedData}
+          margin={{
+            top: 20,
+            right: 75,
+            left: 20,
+            bottom: 80,
+          }}
+        >
+          <CartesianGrid strokeDasharray='3 3' stroke={palette.grey[800]} />
+          <XAxis dataKey='name' tickLine={false} style={{ fontSize: "10px" }}>
+            <Label value='Month' offset={-5} position='insideBottom' />
+          </XAxis>
+          <YAxis
+            domain={[12000, 26000]}
+            axisLine={{ strokeWidth: "0" }}
+            style={{ fontSize: "10px" }}
+            tickFormatter={(v) => `$${v}`}
+          >
+            <Label
+              value='Revenue in USD'
+              angle={-90}
+              offset={-5}
+              position='insideLeft'
+            />
+          </YAxis>
+          <Tooltip />
+          <Legend verticalAlign='top' />
+          <Line
+            type='monotone'
+            dataKey='Actual Revenue'
+            stroke={palette.primary.main}
+            strokeWidth={0}
+            dot={{ strokeWidth: 5 }}
+          />
+          <Line
+            type='monotone'
+            dataKey='Regression Line'
+            stroke='#8884d8'
+            dot={false}
+          />
+          {isPredictions && (
+            <Line
+              strokeDasharray='5 5'
+              dataKey='Predicted Revenue'
+              stroke={palette.secondary[500]}
+            />
+          )}
+        </LineChart>
+      </ResponsiveContainer>
+    </DashboardBox>
   )
 }
 
-export default Navbar
+export default Predictions
